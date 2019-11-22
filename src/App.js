af31e60dc823734ae7eb5934e614fb16b63b4b3c
Manoel Malafaia <mmalafaia@gmail.com>
2019-11-22 15:33:54 -0300

    Commit: React Hooks completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [tech, setTech] = useState([]);
+  const [newTech, setNewTech] = useState('');
+
+  // Só reconstroi a função quando as variáveis 'tech' e 'newTech' são alteradas
+  const handleAdd = useCallback(() => {
+    setTech([...tech, newTech]);
+    setNewTech('');
+  }, [tech, newTech]);
+
+  // funciona como um componentDidMount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('tech');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+
+    // Executa no final como um componentDidUnmount
+    return () => {
+      const a = 'Desmontando';
+
+      alert.alert(a);
+    };
+  }, []);
+
+  // funciona como um componentDidUpdate
+  useEffect(() => {
+    localStorage.setItem('tech', JSON.stringify(tech));
+  }, [tech]);
+
+  // Só executando quando o state 'tech' é alterado
+  const techSize = useMemo(() => tech.length, [tech]);
+
+  return (
+    <>
+      <ul>
+        {tech.map(t => (
+          <li key={t}>{t}</li>
+        ))}
+      </ul>
+      <strong>Você tem {techSize} tecnologias</strong>
+      <br />
+      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <button type="button" onClick={handleAdd}>
+        Adicionar
+      </button>
+    </>
+  );
 }
 
 export default App;
